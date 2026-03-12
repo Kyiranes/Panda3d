8f134760d954c86fd3a0c6d58d009b6da9500e6f
Kyiranes <evilmcchip@gmail.com>
2026-03-12 14:04:21 -0400

    Fixed project 4

--- Chapter 7/Project 4/Project4.py
+++ Chapter 7/Project 4/Project4.py
@@ -1,6 +1,5 @@
-from direct.showbase.ShowBase import ShowBase
+from direct.showbase.ShowBase import ShowBase, taskMgr
 from panda3d.core import Vec3, NodePath, Loader, Texture, PandaNode, CollisionNode,CollisionSphere, CollisionCapsule,CollisionTraverser,CollisionHandlerPusher, CollisionInvSphere
-from CollideObjectBase import *
 from direct.task import Task
 import sys
 import math,random
@@ -48,8 +47,8 @@ class PlacedObject(PandaNode):
           self.modelNode: NodePath = loader.loadModel(modelPath)
           if not isinstance(self.modelNode, NodePath):
                raise AssertionError("PlacedObject loader.loadModel("+ modelPath + ") did not return a proper PandaNode!")
-               self.modelNode.reparentTo(parentNode)
-               self.modelNode.setName(nodeName)
+          self.modelNode.reparentTo(ParentNode)
+          self.modelNode.setName(nodeName)
 class CollidableObject(PlacedObject):
      def __init__(self,loader:Loader,modelPath:str,parentNode: NodePath, nodeName:str):
           super(CollidableObject,self).__init__(loader,modelPath,parentNode,nodeName)
@@ -60,9 +59,9 @@ class InverseSphereCollideObject(CollidableObject):
           super(InverseSphereCollideObject, self).__init__(loader,modelPath,parentNode,nodeName)
           self.collisionNode.node().addSolid(CollisionInvSphere(colPositionVec,colRadius))
 class CapsuleCollidableObject(CollidableObject):
-     def __init__(self,loader:Loader,modelPath:str,parentNode: NodePath, nodeName: str, ax: float, ay: float, az: float, bx: float, by: float, bz:float, r: float)
-     super(CapsuleCollidableObject,self).__init__(loader,modelPath,parentNode,nodeName)
-     self.collisionNode.node().addSolid(CollisionCapsule(ax,ay,az,bz,by,bz,r))
+     def __init__(self,loader:Loader,modelPath:str,parentNode: NodePath, nodeName: str, ax: float, ay: float, az: float, bx: float, by: float, bz:float, r: float):
+          super(CapsuleCollidableObject,self).__init__(loader,modelPath,parentNode,nodeName)
+          self.collisionNode.node().addSolid(CollisionCapsule(ax,ay,az,bz,by,bz,r))
 class SphereCollideObject(CollidableObject):
      def __init__(self, loader:Loader, modelPath:str, parentNode: NodePath, nodeName: str, colPositionVec: Vec3, colRadius: float):
           super(SphereCollideObject,self).__init__(loader,modelPath,parentNode,nodeName)
@@ -72,33 +71,40 @@ class SphereCollideObject(CollidableObject):
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.accept("escape",self.quit)
-        fullCycle = 60
-        for j in range(fullCycle):
-          Drone.droneCount += 1
-          nickName = "Drone" + str(Drone.droneCount)
-          self.DrawCloudDefense(self.Planet1, nickName)
-          self.DrawBaseballSeams(self.Spacestation, nickName, j, fullCycle, 2)
-          self.DrawCircleXDefense(self.Planet2, nickName, j, fullCycle, 2)
-          self.DrawCircleYDefense(self.Planet3, nickName, j, fullCycle, 2)
-          self.DrawCircleZDefense(self.Planet4, nickName, j, fullCycle, 2)
-          self.SetCamera(self.Player)
-          self.cTrav = CollisionTraverser()
-          self.cTrav.traverse(self.render)
-          self.pusher = CollisionHandlerPusher()
-          self.pusher.addCollider(self.Player.collisionNode, self.Player.modelNode)
-          self.cTrav.addCollider(Self.Player.collisionNode, self.pusher)
-          self.cTrav.showCollisions(self.render)
-      def setScene(self):
         self.Universe = PoorlyRenderedUniverse(self.loader, "./Assets/Universe/Universe.x",self.render,"Universe","./Assets/Universe/Universe.jpg",(0,0,0), 10000)
         self.Planet1 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet1/protoPlanet.x",self.render,"Planet1","./Assets/Planets/Planet1/Planet1.png", (1150, 5000, 7), 250)
         self.Planet2 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet2/protoPlanet.x",self.render,"Planet2","./Assets/Planets/Planet2/Planet2.png", (500, 9000, 700), 400)
-        self.Planet3 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet3/protoPlanet.x",self.render,"Planet3","./Assets/Planets/Planet3/Planet3.png", (1000, 2000, 174), 100)
+        self.Planet3 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet3/protoPlanet.x",self.render,"Planet3","./Assets/Planets/Planet3/Planet3.png", (-1000, 2000, 174), 100)
         self.Planet4 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet4/protoPlanet.x",self.render,"Planet4","./Assets/Planets/Planet4/Planet4.png", (850, 7000, 270), 400)
         self.Planet5 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet5/protoPlanet.x",self.render,"Planet5","./Assets/Planets/Planet5/Planet5.png", (450, 3000, 245), 600)
         self.Planet6 = GalaxyPlanet(self.loader,"./Assets/Planets/Planet6/protoPlanet.x",self.render,"Planet6","./Assets/Planets/Planet6/Planet6.png", (1500, 5000, 800), 500)
         self.Player = Player(self.loader, "./Assets/Spaceships/Phaser/phaser.x",self.render,"Player","./Assets/Spaceships/Phaser/phaserII.jpg", (100, 100, 100), 60)
-        self.Spacestation = TheISSIGuess(self.loader, "./Assets/SpaceStation/spaceStation.x", self.render, "Space Station", "./Assets/SpaceStation/SpaceStation1_Dif2.png", (2000, 1000, 250),300)
+        self.Spacestation = TheISSIGuess(self.loader, "./Assets/SpaceStation/spaceStation.x", self.render, "Space Station", "./Assets/SpaceStation/SpaceStation1_Dif2.png", (3000, 2000, 1000),300)
+        self.accept("escape",self.quit)
+        fullCycle = 60
+        self.cTrav = CollisionTraverser()
+        self.cTrav.traverse(self.render)
+        self.pusher = CollisionHandlerPusher()
+        self.pusher.addCollider(self.Player.collisionNode, self.Player.modelNode)
+        self.pusher.addCollider(self.Spacestation.collisionNode, self.Spacestation.modelNode)
+        self.pusher.addCollider(self.Planet1.collisionNode, self.Planet1.modelNode)
+        self.pusher.addCollider(self.Planet2.collisionNode, self.Planet2.modelNode)
+        self.pusher.addCollider(self.Planet3.collisionNode, self.Planet3.modelNode)
+        self.pusher.addCollider(self.Planet4.collisionNode, self.Planet4.modelNode)
+        self.pusher.addCollider(self.Planet5.collisionNode, self.Planet5.modelNode)
+        self.pusher.addCollider(self.Planet6.collisionNode, self.Planet6.modelNode)
+        self.cTrav.addCollider(self.Player.collisionNode, self.pusher)
+        self.cTrav.showCollisions(self.render)
+        for j in range(fullCycle):
+               Drone.droneCount += 1
+               nickName = "Drone" + str(Drone.droneCount)
+               self.DrawCloudDefense(self.Planet1, nickName)
+               self.DrawBaseballSeams(self.Spacestation, nickName, j, fullCycle, 2)
+               self.DrawCircleXDefense(self.Planet2, nickName, j, fullCycle, 2)
+               self.DrawCircleYDefense(self.Planet3, nickName, j, fullCycle, 2)
+               self.DrawCircleZDefense(self.Planet4, nickName, j, fullCycle, 2)
+               self.SetCamera(self.Player)
+    
     
     def quit(self):
             sys.exit()
@@ -106,37 +112,47 @@ class MyApp(ShowBase):
          unitVec = BaseballSeams(step,numDrones, B = 0.4)
          unitVec.normalize()
          position = unitVec * radius * 250 + centralObject.modelNode.getPos()
-         Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         drone = Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+         self.cTrav.addCollider(drone.collisionNode, self.pusher)
     def DrawCloudDefense(self, centralObject, droneName):
          unitVec = Cloud()
          unitVec.normalize()
          position = unitVec * 500 + centralObject.modelNode.getPos()
-         Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 10)
+         drone = Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 10)
+         self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+         self.cTrav.addCollider(drone.collisionNode, self.pusher)
     def DrawCircleZDefense(self, centralObject, droneName, step, numDrones, radius = 1):
          unitVec = CircleZ(step, numDrones)
          unitVec.normalize()
          position = unitVec * radius * 250 + centralObject.modelNode.getPos()
-         Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         drone = Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+         self.cTrav.addCollider(drone.collisionNode, self.pusher)
     def DrawCircleYDefense(self, centralObject, droneName, step, numDrones, radius = 1):
          unitVec = CircleY(step, numDrones)
          unitVec.normalize()
          position = unitVec * radius * 250 + centralObject.modelNode.getPos()
-         Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         drone = Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+         self.cTrav.addCollider(drone.collisionNode, self.pusher)
     def DrawCircleXDefense(self, centralObject, droneName, step, numDrones, radius = 1):
          unitVec = CircleX(step, numDrones)
          unitVec.normalize()
          position = unitVec * radius * 250 + centralObject.modelNode.getPos()
-         Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         drone = Drone(self.loader, "./Assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./Assets/DroneDefender/octotoad1_auv.png", position, 5)
+         self.pusher.addCollider(drone.collisionNode, drone.modelNode)
+         self.cTrav.addCollider(drone.collisionNode, self.pusher)
     def SetCamera(self, player):
          self.disableMouse()
          self.camera.reparentTo(player.modelNode)
          self.camera.setFluidPos(0,1,0)
-
+               
 
 
 class PoorlyRenderedUniverse(InverseSphereCollideObject):
       def __init__(self,loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
-          super(Universe,self).__init__(loader,modelPath,parentNode,nodeName, Vec3(0,0,0),0.9)
+          super(PoorlyRenderedUniverse,self).__init__(loader,modelPath,parentNode,nodeName, Vec3(0,0,0),1.1)
           self.modelNode.setPos(posVec)
           self.modelNode.setScale(scaleVec)
           self.modelNode.setName(nodeName)
@@ -151,7 +167,7 @@ class GalaxyPlanet(SphereCollideObject):
           tex = loader.loadTexture(texPath)
           self.modelNode.setTexture(tex,1)
           
-class Player(SphereCollideObject):
+class Player(SphereCollideObject, ShowBase):
      def __init__(self,loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
           super(Player,self).__init__(loader,modelPath,parentNode,nodeName, Vec3(0,0,0),0.5)
           self.modelNode.setPos(posVec)
@@ -159,9 +175,9 @@ class Player(SphereCollideObject):
           self.modelNode.setName(nodeName)
           tex = loader.loadTexture(texPath)
           self.modelNode.setTexture(tex,1)
-          self.taskManager = taskMgr
           self.render = parentNode
           self.SetKeyBinding()
+          self.taskManager = taskMgr
      def Thrust(self, keyDown):
           if keyDown:
               self.taskManager.add(self.ApplyThrust, "forward-thrust")
@@ -258,17 +274,16 @@ class Player(SphereCollideObject):
 
 class TheISSIGuess(CapsuleCollidableObject):
       def __init__(self,loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
-          super(TheISSIGuess,self).__init__(loader,modelPath,parentNode,nodeName,1,-1,5,1,-1,-5,10)
+          super(TheISSIGuess,self).__init__(loader,modelPath,parentNode,nodeName,1,-1,5,1,-1,-5,3)
           self.modelNode.setPos(posVec)
           self.modelNode.setScale(scaleVec)
           self.modelNode.setName(nodeName)
           tex = loader.loadTexture(texPath)
           self.modelNode.setTexture(tex,1)
-class Drone(ShowBase):
+class Drone(SphereCollideObject):
       droneCount = 0
       def __init__(self,loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
-          self.modelNode = loader.loadModel(modelPath)
-          self.modelNode.reparentTo(parentNode)
+          super(Drone, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0,0,0), 2)
           self.modelNode.setPos(posVec)
           self.modelNode.setScale(scaleVec)
           self.modelNode.setName(nodeName)
